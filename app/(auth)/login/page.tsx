c1c8e443a8554f90c812ac241f6dc9bada5d58d3
Sahithyan <sahithyan2701@gmail.com>
2025-03-09 00:31:52 +0530

    add login page

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,57 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import type React from "react";
+
+const LoginPage: React.FC = () => {
+	return (
+		<div className="flex gap-6 flex-col items-center justify-center min-h-screen">
+			<div>
+				<h1 className="text-4xl font-bold">Welcome to Spirit11</h1>
+				<p className="text-lg">Made by team NullPtr</p>
+			</div>
+			<Card className="w-full max-w-sm">
+				<CardHeader>
+					<h2 className="text-2xl font-bold">Login</h2>
+				</CardHeader>
+				<CardContent>
+					<form>
+						<div className="mb-4">
+							<label
+								htmlFor="username"
+								className="block text-sm font-medium text-gray-700"
+							>
+								Username
+							</label>
+							<Input
+								id="username"
+								type="text"
+								placeholder="Enter your username"
+								className="mt-1 block w-full"
+							/>
+						</div>
+						<div className="mb-6">
+							<label
+								htmlFor="password"
+								className="block text-sm font-medium text-gray-700"
+							>
+								Password
+							</label>
+							<Input
+								id="password"
+								type="password"
+								placeholder="Enter your password"
+								className="mt-1 block w-full"
+							/>
+						</div>
+						<Button type="submit" className="w-full">
+							Login
+						</Button>
+					</form>
+				</CardContent>
+			</Card>
+		</div>
+	);
+};
+
+export default LoginPage;
